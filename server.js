5d26c31ff40b469553268e51433e1924c69e2c8b
JALoquacious <jalikoudis@protonmail.com>
2018-09-17 20:47:34 -0400

    Save data to MongoDB with Mongoose

--- server.js
+++ server.js
@@ -3,30 +3,43 @@ const bodyParser = require('body-parser');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const mongoose = require('mongoose');
+const dbUrl = require('./credentials.config').dbUrl;
 
 const server = http.listen(3000, () => {
     console.log(`Server is listening on port ${server.address().port}.`);
 });
 
-const messages = [
-    { name: 'Joe', content: 'Hello, World, I\'m Joe!' },
-    { name: 'Jan', content: 'Hello, World, I\'m Jan!' }
-];
+const Message = mongoose.model('Message', {
+    name: String,
+    content: String
+});
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/messages', (req, res) => {
-    res.send(messages);
+    Message.find({}, (err, messages) => {
+        res.send(messages);
+    });
 });
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body);
-    io.emit('message', req.body);
-    res.sendStatus(200);
+    const message = new Message(req.body);
+
+    message.save((err) => {
+        if (err) res.sendStatus(500);
+
+        io.emit('message', req.body);
+        res.sendStatus(200);
+    });
 });
 
 io.on('connect', (socket) => {
     console.log('User connected to socket.');
+});
+
+mongoose.connect(dbUrl, { useNewUrlParser: true }, (err) => {
+    console.log(`MongoDB connected. Error?: ${err}`)
 });
